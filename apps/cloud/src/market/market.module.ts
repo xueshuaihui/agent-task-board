554957c09f78acc6646e524a0ab709720462382c
xuesh <xuesh@local>
2026-09-20 23:57:45 +0800

    chore: v0.0.4 W1c 删除服务端市场 apps/cloud 及发布残留

--- apps/cloud/src/market/market.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MarketController } from './market.controller';
-import { MarketService } from './market.service';
-
-@Module({
-  controllers: [MarketController],
-  providers: [MarketService],
-})
-export class MarketModule {}
